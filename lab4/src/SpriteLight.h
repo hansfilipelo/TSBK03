44aeb425acf7e4d80ef2747976dc2b439b5bbebf
Hans-Filip Elo <hansfilipelo@gmail.com>
2016-09-26 09:07:55 +0200

    Skeleton for Lab4

---
+++ lab4/src/SpriteLight.h
@@ -0,0 +1,48 @@
+// SpriteLight - Heavily simplified sprite engine
+// by Ingemar Ragnemalm 2009
+
+// What does a mogwai say when it sees a can of soda?
+// Eeek! Sprite light! Sprite light!
+
+#ifdef __APPLE__
+	#include <OpenGL/gl3.h>
+	#include "../../common/mac/MicroGlut.h"
+#else
+	#include <GL/gl.h>
+	#include "../../common/Linux/MicroGlut.h"
+#endif
+
+#include "../../common/LoadTGA.h"
+#include "../../common/VectorUtils3.h"
+#include "../../common/GL_utilities.h"
+#include <math.h>
+
+typedef struct FPoint
+{
+	GLfloat h, v;
+} FPoint;
+
+typedef struct SpriteRec
+{
+	FPoint position;
+	TextureData *face;
+	FPoint speed;
+	GLfloat rotation;
+	struct SpriteRec *next;
+	
+	// Add custom sprite data here as needed
+} SpriteRec, *SpritePtr;
+
+// Globals: The sprite list, background texture and viewport dimensions (virtual or real pixels)
+extern SpritePtr gSpriteRoot;
+extern GLuint backgroundTexID;
+extern long gWidth, gHeight;
+
+// Functions
+TextureData *GetFace(char *fileName);
+struct SpriteRec *NewSprite(TextureData *f, GLfloat h, GLfloat v, GLfloat hs, GLfloat vs);
+void HandleSprite(SpritePtr sp);
+void DrawSprite(SpritePtr sp);
+void DrawBackground();
+
+void InitSpriteLight();
---
+++ lab4/src/lab4.c
@@ -0,0 +1,127 @@
+// Demo of heavily simplified sprite engine
+// by Ingemar Ragnemalm 2009
+// used as base for lab 4 in TSBK03.
+// OpenGL 3 conversion 2013.
+
+#ifdef __APPLE__
+	#include <OpenGL/gl3.h>
+	#include "../../common/mac/MicroGlut.h"
+	// uses framework Cocoa
+#else
+	#include <GL/gl.h>
+	#include "../../common/Linux/MicroGlut.h"
+#endif
+
+#include <stdlib.h>
+#include "../../common/LoadTGA.h"
+#include "SpriteLight.h"
+#include "../../common/GL_utilities.h"
+
+// Lägg till egna globaler här efter behov.
+
+
+void SpriteBehavior() // Din kod!
+{
+// Lägg till din labbkod här. Det går bra att ändra var som helst i
+// koden i övrigt, men mycket kan samlas här. Du kan utgå från den
+// globala listroten, gSpriteRoot, för att kontrollera alla sprites
+// hastigheter och positioner, eller arbeta från egna globaler.
+}
+
+// Drawing routine
+void Display()
+{
+	SpritePtr sp;
+	
+	glClearColor(0, 0, 0.2, 1);
+	glClear(GL_COLOR_BUFFER_BIT+GL_DEPTH_BUFFER_BIT);
+	glEnable(GL_TEXTURE_2D);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	
+	DrawBackground();
+	
+	SpriteBehavior(); // Din kod!
+	
+// Loop though all sprites. (Several loops in real engine.)
+	sp = gSpriteRoot;
+	do
+	{
+		HandleSprite(sp); // Callback in a real engine
+		DrawSprite(sp);
+		sp = sp->next;
+	} while (sp != NULL);
+	
+	glutSwapBuffers();
+}
+
+void Reshape(int h, int v)
+{
+	glViewport(0, 0, h, v);
+	gWidth = h;
+	gHeight = v;
+}
+
+void Timer(int value)
+{
+	glutTimerFunc(20, Timer, 0);
+	glutPostRedisplay();
+}
+
+// Example of user controllable parameter
+float someValue = 0.0;
+
+void Key(unsigned char key,
+         __attribute__((unused)) int x,
+         __attribute__((unused)) int y)
+{
+  switch (key)
+  {
+    case '+':
+    	someValue += 0.1;
+    	printf("someValue = %f\n", someValue);
+    	break;
+    case '-':
+    	someValue -= 0.1;
+    	printf("someValue = %f\n", someValue);
+    	break;
+    case 0x1b:
+      exit(0);
+  }
+}
+
+void Init()
+{
+	TextureData *sheepFace, *blackFace, *dogFace, *foodFace;
+	
+	LoadTGATextureSimple("img/leaves.tga", &backgroundTexID); // Bakgrund
+	
+	sheepFace = GetFace("img/sheep.tga"); // Ett får
+	blackFace = GetFace("img/blackie.tga"); // Ett svart får
+	dogFace = GetFace("img/dog.tga"); // En hund
+	foodFace = GetFace("img/mat.tga"); // Mat
+	
+	NewSprite(sheepFace, 100, 200, 1, 1);
+	NewSprite(sheepFace, 200, 100, 1.5, -1);
+	NewSprite(sheepFace, 250, 200, -1, 1.5);
+}
+
+int main(int argc, char **argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
+	glutInitWindowSize(800, 600);
+	glutInitContextVersion(3, 2);
+	glutCreateWindow("SpriteLight demo / Flocking");
+	
+	glutDisplayFunc(Display);
+	glutTimerFunc(20, Timer, 0); // Should match the screen synch
+	glutReshapeFunc(Reshape);
+	glutKeyboardFunc(Key);
+	
+	InitSpriteLight();
+	Init();
+	
+	glutMainLoop();
+	return 0;
+}
